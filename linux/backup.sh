1424cc9e627f837793393cc2a5a04afc42f8d2a8
s00p123 <98549069+s00p123@users.noreply.github.com>
2022-01-27 17:21:11 -0600

    Add files via upload

---
+++ linux/backup.sh
@@ -0,0 +1,11 @@
+#This script performs an archival of important Linux OS directories
+
+#!/bin/bash
+cd /root
+mkdir thicc
+cd /
+folders="etc var root home sbin bin"
+for dir in $folders
+do
+	tar czvfp /root/thicc/${dir}.tgz ${dir}
+done

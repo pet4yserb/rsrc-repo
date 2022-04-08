6ad5279ec03c58644620544443e624b909b2ff1c
s00p123 <98549069+s00p123@users.noreply.github.com>
2022-04-08 11:55:25 -0500

    upload

---
+++ linux/inet.sh
@@ -0,0 +1,4 @@
+iptables -I INPUT -p udp --sport 53 -j ACCEPT_LOG
+iptables -I OUTPUT -p udp --dport 53 -j ACCEPT_LOG
+iptables -I INPUT -p tcp -m multiport --sports 80,443 -j ACCEPT_LOG
+iptables -I OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT_LOG

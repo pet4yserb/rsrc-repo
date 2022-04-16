ec7519640d47bb9e3642f91b4509e31f1c63c84e
s00p123 <98549069+s00p123@users.noreply.github.com>
2022-04-16 17:20:35 -0500

    Revert "Merge branch 'windows' of https://github.com/s00p123/resource-repo into windows"
    This reverts commit 9c554b877aba7632a4d6436e96e6e63a6c59e4ba, reversing
    changes made to 01a3cd529cb6a53608c57a6fdebfa20437a784f1.

---
+++ windows/Blueteam-main/pwchange.ps1
@@ -0,0 +1,5 @@
+﻿$OUpath = 'OU=Employees,dc=allsafe,dc=com'
+$users = Get-ADUser -Filter * -SearchBase $OUpath | Select -ExpandProperty sAMAccountName
+foreach ($user in $users) {
+Add-Content -Path C:\Users\Administrator\Desktop\Userpass.txt -Value $user","Password123!
+}

3c32ea000a0a7bfbfbb5b1edeb565aacb732b20d
Anna Andler <aandler@depaul.edu>
2023-01-17 00:10:25 -0600

    Clean up files

--- linux/inet.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-#Allows for outbound internet access
-iptables -I INPUT -p udp --sport 53 -j ACCEPT_LOG
-iptables -I OUTPUT -p udp --dport 53 -j ACCEPT_LOG
-iptables -I INPUT -p tcp -m multiport --sports 80,443 -j ACCEPT_LOG
-iptables -I OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT_LOG
--- linux/nonet.sh
+++
@@ -1,6 +0,0 @@
-#/bin/bash
-#Denies internet access
-iptables -D INPUT -p udp --sport 53 -j ACCEPT_LOG
-iptables -D OUTPUT -p udp --dport 53 -j ACCEPT_LOG
-iptables -D INPUT -p tcp -m multiport --sports 80,443 -j ACCEPT_LOG
-iptables -D OUTPUT -p tcp -m multiport --dports 80,443 -j ACCEPT_LOG
